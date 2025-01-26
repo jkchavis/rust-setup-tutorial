ca6ec1c8cb5da2d2609288104dd338d582beeca0
jkchavis <jkchavis@ad.unc.edu>
2025-01-26 20:20:46 +0000

    final changes

--- hello_423/target/debug/deps/hello_423-d664bd8df1cfde5b.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust-setup-tutorial/hello_423/target/debug/deps/libhello_423-d664bd8df1cfde5b.rmeta: src/main.rs
-
-/workspaces/rust-setup-tutorial/hello_423/target/debug/deps/hello_423-d664bd8df1cfde5b.d: src/main.rs
-
-src/main.rs:
