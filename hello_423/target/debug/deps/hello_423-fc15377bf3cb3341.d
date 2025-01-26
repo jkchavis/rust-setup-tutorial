4a64e0c76cffa3155a20b0c22fd610d95cb179a9
jkchavis <jkchavis@ad.unc.edu>
2025-01-26 16:58:57 +0000

    Completed tutorial

---
+++ hello_423/target/debug/deps/hello_423-fc15377bf3cb3341.d
@@ -0,0 +1,5 @@
+/workspaces/rust-setup-tutorial/hello_423/target/debug/deps/libhello_423-fc15377bf3cb3341.rmeta: src/main.rs
+
+/workspaces/rust-setup-tutorial/hello_423/target/debug/deps/hello_423-fc15377bf3cb3341.d: src/main.rs
+
+src/main.rs:
---
+++ hello_423/target/debug/hello_423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-setup-tutorial/hello_423/target/debug/hello_423: /workspaces/rust-setup-tutorial/hello_423/src/main.rs
